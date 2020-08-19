168a8b55ef13a42b31ed223695bf16239afe4a47
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-19 17:39:41 +0530

    Update fibonacci_last_digit.cpp

--- Algorithmic Tool Box/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ Algorithmic Tool Box/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -1,20 +1,24 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
+#define MOD 1000000007
 typedef long long int ll;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll n,i;
-    cin>>n;
-    ll a[n+1];
-    a[0]=0;
-    a[1]=1;
-    for(i=2;i<=n;i++)
-    {
-        a[i]=(a[i-1]+a[i-2])%10;
-    }
-    cout<<a[n];
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll n,i,f=0,s=1,t;
+	cin>>n;
+	if(n<=1)
+	 cout<<n;
+	else
+	{
+		for(i=2;i<=n;i++)
+		{
+			t=(f+s)%10;
+			f=s;
+			s=t;
+		}
+		cout<<t;
+	}
+	return 0;
 }
