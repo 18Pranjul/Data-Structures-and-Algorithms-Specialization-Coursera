1add0f64bcf7e5b3a84c43edf7c2d83869dbb98a
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-19 17:38:07 +0530

    Update fibonacci.cpp

--- Algorithmic Tool Box/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.cpp
+++ Algorithmic Tool Box/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.cpp
@@ -1,18 +1,24 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
+#define MOD 1000000007
 typedef long long int ll;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll n,i;
-    ll a[55];
-    a[0]=0;
-    a[1]=1;
-    for(i=2;i<=50;i++)
-    a[i]=a[i-1]+a[i-2];
-    cin>>n;
-    cout<<a[n];
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n,i,f=0,s=1,t;
+	cin>>n;
+	if(n<=1)
+	 cout<<n;
+	else
+	{
+		for(i=2;i<=n;i++)
+		{
+			t=f+s;
+			f=s;
+			s=t;
+		}
+		cout<<t;
+	}
+	return 0;
 }
