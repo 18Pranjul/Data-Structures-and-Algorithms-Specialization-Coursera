31ea6559a6bc1f48673df64f22aa7ab26727ab79
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-19 17:37:19 +0530

    Update max_pairwise_product.cpp

--- Algorithmic Tool Box/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ Algorithmic Tool Box/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -1,17 +1,18 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <math.h>
 using namespace std;
+#define MOD 1000000007
 typedef long long int ll;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll n,i;
-    cin>>n;
-    ll a[n];
-    for(i=0;i<n;i++)
-    cin>>a[i];
-    sort(a,a+n);
-    cout<<a[n-1]*a[n-2]<<endl;
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll n,i,a[100005],m1;
+	cin>>n;
+	for(i=0;i<n;i++) cin>>a[i];
+	sort(a,a+n);
+	m1=a[n-1]*a[n-2];
+	cout<<m1;
+	return 0;
 }
