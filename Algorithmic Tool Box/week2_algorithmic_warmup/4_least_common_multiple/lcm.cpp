829d16671265fa6fe1c4c653c40b3908d3a3c664
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-19 17:40:51 +0530

    Update lcm.cpp

--- Algorithmic Tool Box/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
+++ Algorithmic Tool Box/week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
@@ -1,23 +1,21 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
+#define MOD 1000000007
 typedef long long int ll;
+ll gcd(ll a,ll b)
+{
+	if(b==0)
+	 return a;
+	return gcd(b,a%b);
+}
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll x1,y1,a1,b1,d;
-    cin>>a1>>b1;
-    x1=a1;
-    y1=b1;
-    while(1)
-    {
-        d=b1%a1;
-        if(d==0)
-        break;
-        b1=a1;
-        a1=d;
-    }
-    cout<<(x1*y1)/a1<<endl;
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll a,b,c;
+	cin>>a>>b;
+	c=a*b;
+	c/=gcd(a,b);
+	cout<<c;
+	return 0;
 }
