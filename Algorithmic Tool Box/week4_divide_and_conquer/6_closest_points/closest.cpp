b75fd609915ef977c6d15c239a5a469dd0531a19
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-20 18:26:14 +0530

    Update closest.cpp

--- Algorithmic Tool Box/week4_divide_and_conquer/6_closest_points/closest.cpp
+++ Algorithmic Tool Box/week4_divide_and_conquer/6_closest_points/closest.cpp
@@ -1,80 +1,72 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
-struct point  
-{  
-    ll x, y;  
-};  
-bool compareX(point a,point b)
+#define ld long double
+#define MOD 1000000007
+struct point
 {
-    return a.x<b.x;
-}
-bool compareY(point a,point b)
+    ld x;
+    ld y;
+};
+bool cmpx(point p1,point p2)
 {
-    return a.y<b.y;
+    return p1.x<p2.x;
 }
-float distance(point p1,point p2)
+bool cmpy(point p1,point p2)
 {
-    return sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
+    return p1.y<p2.y;
 }
-float hardcalculate(point p[], int n)  
-{  
-    float minimum=FLT_MAX;  
-    for(ll i=0;i<n;i++)  
-        for(ll j=i+1;j<n;j++)  
-            if(distance(p[i],p[j])<minimum)  
-                minimum=distance(p[i],p[j]);  
-    return minimum;  
-}
-float minimum(float x,float y)  
-{  
-    return (x<y)?x:y;  
-} 
-float  finddistance(point strip[], ll n,float d)
+ld dist(point p1,point p2)
 {
-    float minimum=d;
-    sort(strip,strip+n,compareY);
-    for(ll i=0;i<n;i++)  
-        for(ll j=i+1;j<n && (strip[j].y - strip[i].y)<minimum;j++)  
-            if(distance(strip[i],strip[j])<minimum)  
-                minimum=distance(strip[i],strip[j]);  
-    return minimum; 
-    
+    return sqrt(((p1.x-p2.x)*(p1.x-p2.x))+((p1.y-p2.y)*(p1.y-p2.y)));
 }
-float closestpair(struct point p[],ll n)
+ld min(ld a,ld b)
 {
-    if(n<=3)
-    return hardcalculate(p,n);
-    ll mid=n/2;
-    point midpoint=p[mid];
-    float ld=closestpair(p,mid);
-    float rd=closestpair(p+mid,n-mid);
-    float d=min(ld,rd);
-    point strip[n];
-    ll k=0;
-    for(ll i=0;i<n;i++)
-    {
-        if(abs(p[i].x-midpoint.x)<d)
-        {
-            strip[k]=p[i];
-            k++;
-        }
-    }
-    return min(d,finddistance(strip,k,d));
+    return a<b?a:b;
 }
-int main()
+ld ans(point p[],int l,int r)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll n,i;
-    cin>>n;
-    struct point p[n];
-    for(i=0;i<n;i++)
-    {
-        cin>>p[i].x>>p[i].y;
-    }
-    sort(p,p+n,compareX);
-    cout<<fixed<<setprecision(4)<<closestpair(p,n)<<endl;
-    return 0;
+    int i,j;
+    ld mn=DBL_MAX;
+    for(i=l;i<r;i++)
+     for(j=i+1;j<=r;j++)
+      mn=min(mn,dist(p[i],p[j]));
+    return mn;
+}
+ld minDist1(point strip[],int l,ld d)
+{
+    ld mn=d;
+    int i,j;
+    sort(strip,strip+l,cmpy);
+    for(i=0;i<l-1;i++)
+     for(j=i+1;j<l && (strip[j].y-strip[i].y)<mn;j++)
+      mn=min(mn,dist(strip[i],strip[j]));
+    return mn;
+}
+ld minDist(point p[],int l,int r)
+{
+    if(r-l+1<=3)
+     return ans(p,l,r);
+    int i,j=0;
+    int mid=(l+r)/2;
+    ld dl=minDist(p,l,mid);
+    ld dr=minDist(p,mid+1,r);
+    ld d=min(dl,dr);
+    point strip[r-l+5];
+    for(i=l;i<=r;i++)
+     if(abs(p[i].x-p[mid].x)<d)
+      strip[j++]=p[i];
+    return min(d,minDist1(strip,j,d));
+}
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n,i;
+	cin>>n;
+	point p[100005];
+	for(i=0;i<n;i++) cin>>p[i].x>>p[i].y;
+	sort(p,p+n,cmpx);
+	cout<<fixed<<setprecision(3)<<minDist(p,0,n-1);
+	return 0;
 }
