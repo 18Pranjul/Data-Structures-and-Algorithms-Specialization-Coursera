7df1f3ff4c82447c2af3cdd36f892020f348a45e
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-20 18:30:07 +0530

    Update lcs3.cpp

--- Algorithmic Tool Box/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.cpp
+++ Algorithmic Tool Box/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.cpp
@@ -1,43 +1,58 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
+#define MOD 1000000007
 typedef long long int ll;
+ll dp[101][101][101];
+ll max(ll a,ll b)
+{
+    if(a>b)
+     return a;
+    return b;
+}
+ll max1(ll a,ll b,ll c)
+{
+	return max(a,max(b,c));
+}
+void init()
+{
+    ll i,j,k;
+    for(i=0;i<101;i++)
+     for(j=0;j<101;j++)
+      for(k=0;k<101;k++)
+       dp[i][j][k]=0;
+}
+void DP(ll a[],ll b[],ll c[],ll m,ll n,ll k)
+{
+    init();
+    ll i,j,l;
+	for(i=0;i<=m;i++)
+	{
+	    for(j=0;j<=n;j++)
+	    {
+	        for(l=0;l<=k;l++)
+	        {
+	            if(i==0 || j==0 || l==0)
+	             dp[i][j][k]=0;
+	            else if(a[i-1]==b[j-1] && a[i-1]==c[l-1])
+	             dp[i][j][l]=dp[i-1][j-1][l-1]+1;
+	            else
+	             dp[i][j][l]=max1(dp[i-1][j][l],dp[i][j-1][l],dp[i][j][l-1]);
+	        }
+	    }
+	}
+}
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll n,i,j,k,m,l;
-    cin>>n;
-    ll arr1[n+1];
-    arr1[0]=0;
-    for(i=1;i<=n;i++)
-    cin>>arr1[i];
-    cin>>m;
-    ll arr2[m+1];
-    arr2[0]=0;
-    for(i=1;i<=m;i++)
-    cin>>arr2[i];
-    cin>>l;
-    ll arr3[l+1];
-    arr3[0]=0;
-    for(i=1;i<=l;i++)
-    cin>>arr3[i];
-    ll dp[n+1][m+1][l+1];
-    for(i=0;i<=n;i++)
-    {
-        for(j=0;j<=m;j++)
-        {
-            for(k=0;k<=l;k++)
-            {
-                if(i==0 || j==0 || k==0)
-                dp[i][j][k]=0;
-                else
-                if(arr1[i]==arr2[j] && arr1[i]==arr3[k])
-                dp[i][j][k]=dp[i-1][j-1][k-1]+1;
-                else
-                dp[i][j][k]=max(dp[i][j-][k],max(dp[i-1][j][k],dp[i][j][k-1]));
-            }
-        }
-    }
-    cout<<dp[n][m][l]<<endl;;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll m,n,k,a[101],b[101],c[101],i,j,l=0,d[101];
+	cin>>m;
+	for(i=0;i<m;i++) cin>>a[i];
+	cin>>n;
+	for(i=0;i<n;i++) cin>>b[i];
+	cin>>k;
+	for(i=0;i<k;i++) cin>>c[i];
+	DP(a,b,c,m,n,k);
+	cout<<dp[m][n][k];
+	return 0;
 }
