06c2c1f04514cd83f0098ee04af53a6c46928082
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-20 18:19:32 +0530

    Update largest_number.cpp

--- Algorithmic Tool Box/week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
+++ Algorithmic Tool Box/week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
@@ -1,40 +1,24 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <string>
 using namespace std;
+#define MOD 1000000007
 typedef long long int ll;
+bool cmp(string s1,string s2)
+{
+	string a=s1.append(s2);
+	string b=s2.append(s1);
+	return a.compare(b)>0?true:false;
+}
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll n,i,k,x,y,j;
-    cin>>n;
-    string s[n];
-    for(i=0;i<n;i++)
-    {
-        cin>>s[i];
-    }
-    sort(s,s+n);
-    /*for(i=0;i<n;i++)
-    {
-        cout<<s[i]<<" ";
-    }
-    cout<<endl;*/
-    k=n-1;
-    if(k<0)
-    return 0;
-    for(i=9;i>=0;i--)
-    {
-        string ab=to_string(i);
-        x=lower_bound(s,s+n,ab)-s;
-        y=upper_bound(s,s+n,ab)-s;
-        if(binary_search(s,s+n,ab)==1)
-        {
-            for(j=x;j<y;j++)
-            cout<<s[j];
-        }
-        for(j=k;j>=y;j--)
-        cout<<s[j];
-        k=x-1;
-    }
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n,i;
+	string k[101];
+	cin>>n;
+	for(i=0;i<n;i++) cin>>k[i];
+	sort(k,k+n,cmp);
+	for(i=0;i<n;i++) cout<<k[i];
+	return 0;
 }
