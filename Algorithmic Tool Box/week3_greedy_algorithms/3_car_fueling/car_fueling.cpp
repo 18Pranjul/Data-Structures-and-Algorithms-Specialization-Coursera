4851271bbd0d8c848e6a3588e97aaea8ad27987f
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-20 18:15:54 +0530

    Update car_fueling.cpp

--- Algorithmic Tool Box/week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
+++ Algorithmic Tool Box/week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
@@ -1,38 +1,35 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+#define MOD 1000000007
 typedef long long int ll;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll m,n,d,numrefill,currrefill,lastrefill,i;
-    cin>>d>>m>>n;
-    ll a[n+2];
-    a[0]=0;
-    for(i=1;i<=n;i++)
-    cin>>a[i];
-    a[n+1]=d;
-    numrefill=0;
-    currrefill=0;
-    while(currrefill<=n)
-    {
-        lastrefill=currrefill;
-        while(currrefill<=n && (a[currrefill+1]-a[lastrefill]<=m))
-        {
-            currrefill=currrefill+1;
-        }
-        if(currrefill==lastrefill)
-        {
-            cout<<"-1"<<endl;
-            return 0;
-        }
-        else 
-        if(currrefill<=n)
-        {
-            numrefill=numrefill+1;
-        }
-    }
-    cout<<numrefill<<endl;
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int d,l,n,x[100005],i,N=0;
+	cin>>d>>l>>n;
+	x[0]=0;
+	x[n+1]=d;
+	for(i=1;i<=n;i++)
+	 cin>>x[i];
+	sort(x,x+n+2);
+	i=0;
+	while(i<=n)
+	{
+		int k=i;
+		while(i<=n+1 && x[i]-x[k]<=l)
+		 i++;
+		i--;
+		if(k==i)
+		{
+			N=-1;
+			break;
+		}
+		else
+		 if(i<=n)
+		  N++;
+	}
+	cout<<N;
+	return 0;
 }
