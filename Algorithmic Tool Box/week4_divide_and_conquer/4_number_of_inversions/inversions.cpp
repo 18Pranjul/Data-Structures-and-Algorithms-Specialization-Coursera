99e006bea384d324cc78e03211ffcc5d4171af0f
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-20 18:25:11 +0530

    Update inversions.cpp

--- Algorithmic Tool Box/week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
+++ Algorithmic Tool Box/week4_divide_and_conquer/4_number_of_inversions/inversions.cpp
@@ -1,67 +1,54 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 typedef long long int ll;
-ll merge( long long int a[],long long int l,long long int m,long long int r)
+ll c;
+void mer(ll a[],ll n,ll l,ll m,ll r)
 {
-	long long int i,j,k,cnt=0;
-	long long int n1=m-l+1;
-	long long int n2=r-m;
-	 long long int L[n1],R[n2];
-	for(i=0;i<n1;i++)
-	L[i]=a[l+i];
-	for(j=0;j<n2;j++)
-	R[j]=a[m+1+j];
-	i=0;j=0;k=l;
-	while(i<n1 && j<n2)
-	{
-		if(L[i]<=R[j])
-		{
-			a[k]=L[i];
-			i++;
-		}
-		else
-		{
-			a[k]=R[j];
-			j++;
-			cnt=cnt+(n1-i);
-		}
-		k++;
-	}
-	while(i<n1)
-	{
-		a[k]=L[i];
-		i++;
-		k++;
-	}
-	while(j<n2)
-	{
-		a[k]=R[j];
-		j++;
-		k++;
-	}
-	return cnt;
+    ll i,j,k,n1,n2;
+    n1=m-l+1;
+    n2=r-m;
+    ll L[n1],R[n2];
+    for(i=0;i<n1;i++)
+     L[i]=a[l+i];
+    for(j=0;j<n2;j++)
+     R[j]=a[m+j+1];
+    i=j=0;
+    k=l;
+    while(i<n1 && j<n2)
+    {
+        if(L[i]<=R[j])
+         a[k++]=L[i++];
+        else
+        {
+            a[k++]=R[j++];
+            c+=(m+j+1-k);
+        }
+    }
+    while(i<n1)
+     a[k++]=L[i++];
+    while(j<n2)
+     a[k++]=R[j++];
 }
-ll mergesort(long long int a[],long long int l,long long int r)
+void msort(ll a[],ll n,ll l,ll r)
 {
-    ll cnt=0;
-	if(l<r)
-	{
-		long long int m=l+(r-l)/2;
-		cnt=mergesort(a,l,m);
-		cnt+=mergesort(a,m+1,r);
-		cnt+=merge(a,l,m,r);
-	}
-	return cnt;
+    if(l<r)
+    {
+        ll m=l+(r-l)/2;
+        msort(a,n,l,m);
+        msort(a,n,m+1,r);
+        mer(a,n,l,m,r);
+    }
 }
-int main()
+int main() 
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
-    ll n,i;
+    ll n,a[100001],i;
     cin>>n;
-    ll a[n];
-    for(i=0;i<n;i++)
-    cin>>a[i];
-    cout<<mergesort(a,0,n-1)<<endl;
+	for(i=0;i<n;i++) cin>>a[i];
+    c=0;
+    msort(a,n,0,n-1);
+    cout<<c;
+	return 0;
 }
+
