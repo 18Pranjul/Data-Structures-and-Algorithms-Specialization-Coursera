2cd17b5b790c78b209e79de4bff6aaab6aa7a35c
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-20 18:18:49 +0530

    Update different_summands.cpp

--- Algorithmic Tool Box/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.cpp
+++ Algorithmic Tool Box/week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.cpp
@@ -1,33 +1,25 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
+#define MOD 1000000007
 typedef long long int ll;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll n,k,x,y,i;
-    cin>>n;
-    if(n==1)
-    {
-        cout<<"1"<<endl;
-        cout<<"1"<<" ";
-        return 0;
-    }
-    else
-    if(n==2)
-    {
-        cout<<"1"<<endl;
-        cout<<"2"<<" ";
-        return 0;
-    }
-    k=(-1+(ll)sqrt(n*8+1))/2;
-    cout<<k<<endl;
-    for(i=1;i<k;i++)
-    cout<<i<<" ";
-    x=((k-1)*k)/2;
-    y=n-x;
-    cout<<y<<" ";
-    cout<<endl;
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	vector <ll> v;
+	v.push_back(0);
+	ll n,i=1;
+	cin>>n;
+	while(n>=i)
+	{
+		v.push_back(i);
+		n-=i;
+		i++;
+	}
+	v[i-1]+=n;
+	cout<<v.size()-1<<"\n";
+	for(i=1;i<v.size();i++)
+	 cout<<v[i]<<" ";
+	return 0;
 }
