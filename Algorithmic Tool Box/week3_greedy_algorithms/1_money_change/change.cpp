e891d5b4460197c578f008881003487bed001ec0
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-20 18:14:30 +0530

    Update change.cpp

--- Algorithmic Tool Box/week3_greedy_algorithms/1_money_change/change.cpp
+++ Algorithmic Tool Box/week3_greedy_algorithms/1_money_change/change.cpp
@@ -1,20 +1,17 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
+#define MOD 1000000007
 typedef long long int ll;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll n,x,s=0;
-    cin>>n;
-    x=n/10;
-    s+=x;
-    n=n-(10*x);
-    x=n/5;
-    s+=x;
-    n=n-(5*x);
-    s+=n;
-    cout<<s<<endl;
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int m,a,b;
+	cin>>m;
+	a=m/10;
+	m-=(a*10);
+	b=m/5;
+	m-=(b*5);
+	cout<<a+b+m;
+	return 0;
 }
