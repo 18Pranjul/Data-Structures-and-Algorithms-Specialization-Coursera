b43d5f5969a04f2a80cf350c083e42f3525e2be6
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-20 18:22:20 +0530

    Update binary_search.cpp

--- Algorithmic Tool Box/week4_divide_and_conquer/1_binary_search/binary_search.cpp
+++ Algorithmic Tool Box/week4_divide_and_conquer/1_binary_search/binary_search.cpp
@@ -1,29 +1,33 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+#define MOD 1000000007
 typedef long long int ll;
+ll bin(ll a[],ll l,ll r,ll x)
+{
+	if(l>r)
+	 return -1;
+	ll m=(l+r)/2;
+	if(a[m]==x)
+	 return m;
+	if(a[m]>x)
+	 bin(a,l,m-1,x);
+	else
+	 bin(a,m+1,r,x);
+}
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll n,k,i,x;
-    cin>>n;
-    ll a[n];
-    for(i=0;i<n;i++)
-    cin>>a[i];
-    cin>>k;
-    ll b[k];
-    for(i=0;i<k;i++)
-    cin>>b[i];
-    for(i=0;i<k;i++)
-    {
-        if(binary_search(a,a+n,b[i])==1)
-        {
-            x=lower_bound(a,a+n,b[i])-a;
-            cout<<x<<" ";
-        }
-        else
-        cout<<"-1"<<" ";
-    }
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll n,k,b,a[100001],i;
+	cin>>n;
+	for(i=0;i<n;i++) cin>>a[i];
+	cin>>k;
+	sort(a,a+n);
+	for(i=0;i<k;i++)
+	{
+		cin>>b;
+		cout<<bin(a,0,n-1,b)<<" ";
+	}
+	return 0;
 }
