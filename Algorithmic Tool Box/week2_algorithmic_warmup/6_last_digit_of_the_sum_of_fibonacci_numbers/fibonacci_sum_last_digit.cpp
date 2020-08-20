7c749c978d00ff32c9c841f0f53d09d17919df55
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-20 18:11:47 +0530

    Update fibonacci_sum_last_digit.cpp

--- Algorithmic Tool Box/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
+++ Algorithmic Tool Box/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
@@ -1,30 +1,47 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
+#define MOD 1000000007
 typedef long long int ll;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll n,i,x;
-    cin>>n;
-    ll a[65],s[65];
-    a[0]=0;
-    a[1]=1;
-    s[0]=0;
-    s[1]=1;
-    for(i=2;i<=60;i++)
-    {
-        a[i]=(a[i-1]+a[i-2])%10;
-        s[i]=(s[i-1]+a[i])%10;
-    }
-    if(n==0)
-    cout<<"0"<<endl;
-    else
-    {
-        x=n%60;
-        cout<<(s[x])%10<<endl;
-    }
-    
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll n,f=0,s=1,t,sum=1,i,j=0,k;
+	cin>>n;
+	vector <ll> v;
+	v.push_back(0);
+	v.push_back(1);
+	if(n<=1)
+	 cout<<n;
+	else
+	{
+		for(i=2;;i++)
+		{
+			t=(f+s)%10;
+			sum=(sum+t)%10;
+			f=s;
+			s=t;
+			//cout<<sum<<endl;
+			v.push_back(sum%10);
+			if(v[i]==v[j])
+			{
+				j++;
+				if((i+1)/2==j && i%2==1)
+				 break;
+			}
+			else
+			{
+				j=0;
+				if(v[i]==v[j])
+				{
+					j++;
+				}
+			}
+		}
+		//cout<<"Period: "<<j<<endl;
+		n%=j;
+		cout<<v[n];
+	}
+	return 0;
 }
