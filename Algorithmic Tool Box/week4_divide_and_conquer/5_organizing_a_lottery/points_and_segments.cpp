23124074e1373794824882379b7b2a2fce6384ef
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-20 18:25:42 +0530

    Update points_and_segments.cpp

--- Algorithmic Tool Box/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
+++ Algorithmic Tool Box/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
@@ -1,76 +1,58 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+#define MOD 1000000007
 typedef long long int ll;
-struct roop
+struct st
 {
-    ll x;
-    ll y;
+	ll n;
+	ll pos;
+	ll pos1;
 };
-bool comp2(roop a, roop b)
+bool cmp(st s1,st s2)
 {
-    return a.x<b.x;
-}
-bool comp(roop a ,roop b)
-{
-    if(a.x==b.x)
-    {
-        return a.y<b.y;
-    }
-    else
-    return a.x<b.x;
+	if(s1.n==s2.n)
+	 return s1.pos<s2.pos;
+	return s1.n<s2.n;
 }
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    ll n,k,i,a,b,c,l;
-    cin>>n>>k;
-    struct roop rup[k+1];
-    struct roop r[2*n+k+1];
-    ll arr[k+1];
-    //cout<<n<<k;
-    for(i=0;i<n;i++)
-    {
-        cin>>a>>b;
-        r[2*i].x=a;
-        r[2*i].y=1;
-        r[2*i+1].x=b;
-        r[2*i+1].y=3;
-    }
-    for(i=0;i<k;i++)
-    {
-        cin>>c;
-        r[2*n+i].x=c;
-        r[2*n+i].y=2;
-        rup[i].x=c;
-        rup[i].y=i;
-    }
-    sort(rup,rup+k,comp2);
-    sort(r,r+(2*n+k),comp);
-    c=0;
-    l=0;
-    for(i=0;i<(2*n+k);i++)
-    {
-        if(r[i].y==1)
-        c++;
-        else
-        if(r[i].y==3)
-        c--;
-        else
-        if(r[i].y==2)
-        {
-            arr[rup[l++].y]=c;
-        }
-    }
-    for(i=0;i<k;i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-    /*for(i=0;i<(2*n+k);i++)
-    cout<<r[i].x<<" "<<r[i].y<<endl;*/
-    return 0;
-    
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll s,p,i,a,b,j,k,l,r;
+	cin>>s>>p;
+	st seg[(s*2)+p];
+	ll x,ans[p+1];
+	j=0;
+	for(i=0;i<s;i++)
+	{
+		cin>>a>>b;
+		seg[j].n=a;
+		seg[j++].pos=-1;
+		seg[j].n=b;
+		seg[j++].pos=1;
+	}
+	i=0;
+	for(i=0;i<p;i++)
+	{
+		cin>>x;
+		seg[j].n=x;
+		seg[j].pos=0;
+		seg[j++].pos1=i;
+	}
+	sort(seg,seg+j,cmp);
+	k=l=r=0;
+	while(k<j)
+	{
+		if(seg[k].pos==0)
+		 ans[seg[k].pos1]=l-r;
+		if(seg[k].pos==-1)
+		 l++;
+		if(seg[k].pos==1)
+		 r++;
+		k++;
+	}
+	for(i=0;i<p;i++)
+	 cout<<ans[i]<<" ";
+	return 0;
 }
