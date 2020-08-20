ca0d1d07a2bae9e24a14d9c4376fd1e45cabd223
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-20 18:15:04 +0530

    Update fractional_knapsack.cpp

--- Algorithmic Tool Box/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ Algorithmic Tool Box/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,52 +1,41 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
 using namespace std;
+#define MOD 1000000007
 typedef long long int ll;
-struct roop
+struct bag
 {
-    ll v;
-    ll w;
-    long double p;
+	ll w;
+	ll v;
+	double r;
 };
-bool comp (roop a,roop b)
+bool cmp(bag b1,bag b2)
 {
-    return a.p>b.p;
+	return b1.r>b2.r;
 }
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll n,W,i;
-    long double value;
-    cin>>n>>W;
-    struct roop a[n];
-    for(i=0;i<n;i++)
-    {
-        cin>>a[i].v;
-        cin>>a[i].w;
-        a[i].p=(long double)((1.0*a[i].v)/a[i].w);
-    }
-    sort(a,a+n,comp);
-    value=0;
-    for(i=0;i<n;i++)
-    {
-        if(W<=0)
-        {
-            break;
-        }
-        else
-        if(a[i].w<=W)
-        {
-            W=W-a[i].w;
-            value+=(1.0*a[i].v);
-        }
-        else
-        if(a[i].w>W)
-        {
-            value+=(W*1.0*a[i].p);
-            W=W-W;
-        }
-    }
-    cout<<fixed<<setprecision(4)<<value<<endl;
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll n,W,i,a;
+	double v=0.0;
+	bag b[1001];
+	cin>>n>>W;
+	for(i=0;i<n;i++)
+	{
+		cin>>b[i].v>>b[i].w;
+		b[i].r=(b[i].v*(1.0))/b[i].w;
+	}
+	sort(b,b+n,cmp);
+	i=0;
+	while(W>0 && i<n)
+	{
+		a=min(b[i].w,W);
+		v+=(a*b[i].v*(1.0))/b[i].w;
+		W-=a;
+		i++;
+	}
+	cout<<fixed<<setprecision(3)<<v;
+	return 0;
 }
