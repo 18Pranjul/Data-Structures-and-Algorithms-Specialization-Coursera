f1e688ecfbfeff0bd832be8ed1957d35d2f40cdf
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-20 18:22:52 +0530

    Update majority_element.cpp

--- Algorithmic Tool Box/week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ Algorithmic Tool Box/week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -1,43 +1,24 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+#define MOD 1000000007
 typedef long long int ll;
+ll maj(ll a[],ll n)
+{
+	ll i;
+	for(i=0;i<n/2+n%2;i++)
+	 if(a[i]==a[i+n/2])
+	  return 1;
+	return 0;
+}
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll n,x,s,i;
-    vector<ll> v;
-    cin>>n;
-    ll a[n];
-    for(i=0;i<n;i++)
-    cin>>a[i];
-    sort(a,a+n);
-    s=1;
-    for(i=1;i<n;i++)
-    {
-        if(a[i]==a[i-1])
-        {
-            s++;
-        }
-        else
-        {
-            v.push_back(s);
-            s=1;
-        }
-    }
-    v.push_back(s);
-    /*for(i=0;i<v.size();i++)
-    cout<<v[i]<<" ";
-    cout<<endl;*/
-    for(i=0;i<v.size();i++)
-    {
-        if(v[i]>(n/2))
-        {
-            cout<<"1"<<endl;
-            return 0;
-        }
-    }
-    cout<<"0"<<endl;
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll n,i,a[100001];
+	cin>>n;
+	for(i=0;i<n;i++) cin>>a[i];
+	sort(a,a+n);
+	cout<<maj(a,n);
+	return 0;
 }
