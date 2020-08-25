0269ae14fd02811e78fd5768d4642bda967934c0
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-25 23:56:14 +0530

    Update build_heap.cpp

--- Data Structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.cpp
+++ Data Structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.cpp
@@ -1,50 +1,43 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 typedef long long int ll;
-vector<ll> b;
+#define MOD 1000000007
+#define MAX 1000000000000000000
+#define ln "\n"
+vector <pair<ll,ll>> v;
 ll c=0;
-void siftdown(ll a[],ll n,ll i)
+void heapify(ll a[],ll n,ll i)
 {
-    ll minindex=i;
-    ll left=2*i+1;
-    ll right=2*i+2;
-    if(left<n && a[left]<a[minindex])
-    minindex=left;
-    if(right<n && a[right]<a[minindex])
-    minindex=right;
-    if(i!=minindex)
+    ll min=i,l=2*i+1,r=2*i+2;
+    if(l<n && a[min]>a[l])
+     min=l;
+    if(r<n && a[min]>a[r])
+     min=r;
+    if(min!=i)
     {
-        b.push_back(i);
-        b.push_back(minindex);
-        swap(a[i],a[minindex]);
+        swap(a[i],a[min]);
+        v.push_back({i,min});
         c++;
-        siftdown(a,n,minindex);
-    }
-    
-}
-void build_heap(ll a[],ll n)
-{
-    ll start=(n/2)-1;
-    for(ll i=start;i>=0;i--)
-    {
-        siftdown(a,n,i);
+        heapify(a,n,min);
     }
 }
-int main()
+void minheap(ll a[],ll n)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll n,i;
-    cin>>n;
-    ll a[n];
-    for(i=0;i<n;i++)
-    cin>>a[i];
-    build_heap(a,n);
-    cout<<c<<endl;
-    for(i=0;i<b.size();i+=2)
-    {
-        cout<<b[i]<<" "<<b[i+1]<<endl;
-    }
-    return 0;
+    ll i;
+    for(i=(n/2)-1;i>=0;i--)
+     heapify(a,n,i);
+}
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll n,a[100005],i;
+	cin>>n;
+	for(i=0;i<n;i++) cin>>a[i];
+	minheap(a,n);
+	cout<<c;
+	for(i=0;i<c;i++) cout<<ln<<v[i].first<<" "<<v[i].second;
+	return 0;
 }
