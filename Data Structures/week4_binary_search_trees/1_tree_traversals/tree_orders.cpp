619dd7b3d18055592b8692043d70bc4136690886
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-26 00:00:22 +0530

    Update tree_orders.cpp

--- Data Structures/week4_binary_search_trees/1_tree_traversals/tree_orders.cpp
+++ Data Structures/week4_binary_search_trees/1_tree_traversals/tree_orders.cpp
@@ -1,149 +1,62 @@
 #include <iostream>
-
 #include <algorithm>
-
 #include <vector>
-
 #include <math.h>
-
 #include <cstring>
-
 #include <string>
-
 #include <stack>
-
 #include <queue>
-
 #include <deque>
-
 #include <map>
-
 #include <set>
-
 #include <utility>
-
 #include <iomanip>
-
 #include <climits>
-
 using namespace std;
-
 #define ll long long
-
 #define MOD 1000000007
-
 #define MAX 1000000000000000000
-
 #define ln "\n"
-
 #define pb push_back
-
 #define pll pair<ll,ll>
-
+#define mp make_pair
 #define f first
 #define s second
-
-#define TEST ll t;cin>>t; while(t--)
-
+#define Test ll t;cin>>t; while(t--)
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
-
-struct roop
-
+struct node
 {
-
-    ll key;
-
-    ll left;
- 
-    ll right;
-
+    ll key,left,right;
 };
-
-ll n;
-
-struct roop r[100001];
-
-void in_order(ll vertex)
-
+void inorder(node nd[],ll n,ll root)
 {
-
-    if(vertex!=-1)
-
-    {
-
-        in_order(r[vertex].left);
-
-        cout<<r[vertex].key<<" ";
-
-        in_order(r[vertex].right);
-
-    }
-
+    if(nd[root].left!=-1) inorder(nd,n,nd[root].left);
+    cout<<nd[root].key<<" ";
+    if(nd[root].right!=-1) inorder(nd,n,nd[root].right);
 }
-
-void pre_order(ll vertex)
-
+void preorder(node nd[],ll n,ll root)
 {
-
-    if(vertex!=-1)
-
-    {
-
-        cout<<r[vertex].key<<" ";
- 
-        pre_order(r[vertex].left);
-
-        pre_order(r[vertex].right);
-
-    }
-
+    cout<<nd[root].key<<" ";
+    if(nd[root].left!=-1) preorder(nd,n,nd[root].left);
+    if(nd[root].right!=-1) preorder(nd,n,nd[root].right);
 }
-void post_order(ll vertex)
-
+void postorder(node nd[],ll n,ll root)
 {
-    if(vertex!=-1)
-
-    {
-
-        post_order(r[vertex].left);
-
-        post_order(r[vertex].right);
-
-        cout<<r[vertex].key<<" ";
-
-    }
+    if(nd[root].left!=-1) postorder(nd,n,nd[root].left);
+    if(nd[root].right!=-1) postorder(nd,n,nd[root].right);
+    cout<<nd[root].key<<" ";
 }
-
 int main() 
-
 {
-
 	fast_io;
-
-	ll i;
-
+	ll n,i;
 	cin>>n;
-
-	for(i=0;i<n;i++)
-
-	{
-
-	    cin>>r[i].key>>r[i].left>>r[i].right;
-
-	}
-
-	in_order(0);
-
-	cout<<endl;
-
-	pre_order(0);
-
-	cout<<endl;
-
-	post_order(0);
-
-	cout<<endl;
-
+	node nd[n+5];
+	for(i=0;i<n;i++) cin>>nd[i].key>>nd[i].left>>nd[i].right;
+	inorder(nd,n,0);
+	cout<<ln;
+	preorder(nd,n,0);
+	cout<<ln;
+	postorder(nd,n,0);
 	return 0;
-
 }
