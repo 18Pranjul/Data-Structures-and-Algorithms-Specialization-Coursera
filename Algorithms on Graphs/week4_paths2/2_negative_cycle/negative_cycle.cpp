ff8535bef7c92817e0b9387e254362bc1357d73c
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-26 00:16:07 +0530

    Update negative_cycle.cpp

--- Algorithms on Graphs/week4_paths2/2_negative_cycle/negative_cycle.cpp
+++ Algorithms on Graphs/week4_paths2/2_negative_cycle/negative_cycle.cpp
@@ -1,71 +1,66 @@
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long int ll;
-#define INF 100000000000
-struct edge
-{
-    ll u,v,cost;
-};
-ll bellman_ford(struct edge e[],ll n,ll m,ll s,ll visited[])
-{
-    vector<ll> dist(n+1,INF);
-    dist[s]=0;
-    ll x;
-    for(ll i=0;i<n;i++)
+#include <bits/stdc++.h> 
+using namespace std; 
+struct Edge { 
+	int src, dest, weight; 
+}; 
+struct Graph { 
+	int V, E; 
+	struct Edge* edge; 
+}; 
+struct Graph* createGraph(int V, int E) 
+{ 
+	struct Graph* graph = new Graph; 
+	graph->V = V; 
+	graph->E = E; 
+	graph->edge = new Edge[graph->E]; 
+	return graph; 
+} 
+int main() 
+{ 
+	int V,E,i,j,k;
+	cin>>V>>E;
+	struct Graph* graph = createGraph(V, E); 
+    for(i=0;i<E;i++) 
     {
-        x=-1;
-        for(ll j=0;j<m;j++)
-        {
-            if(dist[e[j].u]<INF)
-            {
-                if(dist[e[j].v]>dist[e[j].u]+e[j].cost)
-                {
-                    dist[e[j].v]=dist[e[j].u]+e[j].cost;
-                    x=e[j].v;
-                }
-            }
-        }
+        cin>>graph->edge[i].src>>graph->edge[i].dest>>graph->edge[i].weight;
+        graph->edge[i].src--;
+        graph->edge[i].dest--;
     }
-    for(ll i=1;i<=n;i++)
-    {
-        if(dist[i]!=INF)
-        visited[i]=1;
-    }
-    if(x==-1)
-    return 0;
-    else
-    return 1;
-}
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    ll n,m,i,flag=0,x;
-    cin>>n>>m;
-    ll visited[n+1]={0};
-    struct edge e[m+1];
-    for(i=0;i<m;i++)
-    {
-        cin>>e[i].u>>e[i].v>>e[i].cost;
-    }
-    for(i=1;i<=n;i++)
-    {
-        if(visited[i]==0)
-        {
-            x=bellman_ford(e,n,m,i,visited);
-            if(x==1)
-            {
-                flag=1;
-                break;
-            }
-        }
-    }
-    if(flag==1)
-    cout<<1<<endl;
-    else
-    cout<<0<<endl;
-    
-    return 0;
-}
+	bool visited[V+5]; 
+	memset(visited, 0, sizeof(visited)); 
+	int dist[V+5],f=0; 
+	for (int k = 0; k < V; k++) 
+	{ 
+		if (visited[k] == false) 
+		{ 
+			for (int i = 0; i < V; i++) dist[i] = INT_MAX; 
+	        dist[k] = 0; 
+	        for (int i = 1; i <= V - 1; i++) 
+	        { 
+		        for (int j = 0; j < E; j++) 
+		        { 
+			        int u = graph->edge[j].src; 
+			        int v = graph->edge[j].dest; 
+			        int weight = graph->edge[j].weight; 
+			        if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) 
+				     dist[v] = dist[u] + weight; 
+		        } 
+	        } 
+	        for (int i = 0; i < E; i++) 
+	        { 
+		        int u = graph->edge[i].src; 
+		        int v = graph->edge[i].dest; 
+		        int weight = graph->edge[i].weight; 
+		        if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) 
+	            {
+	                f=1;
+	                goto end;
+	            }
+	        }
+			for (int i = 0; i < V; i++) if (dist[i] != INT_MAX)	visited[i]=true;
+		} 
+	} 
+    end:;
+    cout<<f;
+	return 0; 
+} 
