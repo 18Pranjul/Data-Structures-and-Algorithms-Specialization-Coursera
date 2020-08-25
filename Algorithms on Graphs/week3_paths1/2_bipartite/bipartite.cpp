f1fa4506346322546e859d3fcd70c8553d01b503
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-26 00:14:49 +0530

    Update bipartite.cpp

--- Algorithms on Graphs/week3_paths1/2_bipartite/bipartite.cpp
+++ Algorithms on Graphs/week3_paths1/2_bipartite/bipartite.cpp
@@ -1,55 +1,70 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+#include <cstring>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <utility>
+#include <iomanip>
+#include <climits>
 using namespace std;
-typedef long long int ll;
-int main()
+#define ll long long
+#define MOD 1000000007
+#define MAX 1000000000000000000
+#define ln "\n"
+#define pb push_back
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define sll stack<ll>
+#define qll queue<ll>
+#define mp make_pair
+#define f first
+#define s second
+#define Test ll t;cin>>t; while(t--)
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
+int main() 
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll t,n,i,j,z,x,flag,m,a,b,k=1;
-    cin>>n>>m;
-    vector<ll> adj[n+1];
-    for(i=0;i<m;i++)
-    {
-        cin>>a>>b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    vector<ll> color(n+1,-1);
-    queue<ll> q;
-    flag=1;
-    for(i=1;i<=n;i++)
-    {
-        if(color[i]==-1)
+	fast_io;
+	ll n,m,i,u,v;
+	cin>>n>>m;
+	vll ad[n+5];
+	ll vis[n+5]={0},clr[n+5],c=1;
+	for(i=0;i<m;i++)
+	{
+	    cin>>u>>v;
+	    ad[u].pb(v);
+	    ad[v].pb(u);
+	}
+	qll q;
+	q.push(1);
+	vis[1]=1;
+	clr[1]=0;
+	while(q.size())
+	{      
+	    ll z=q.front();
+        q.pop();
+        for(i=0;i<ad[z].size();i++)
         {
-            color[i]=0;
-            q.push(i);
-            while(!q.empty())
+            ll y=ad[z][i];
+            if(vis[y] && clr[y]==clr[z])
             {
-                x=q.front();
-                for(j=0;j<adj[x].size();j++)
-                {
-                    if(color[adj[x][j]]==-1)
-                    {
-                        color[adj[x][j]]=color[x]^1;
-                        q.push(adj[x][j]);
-                    }
-                    else
-                    {
-                        if(color[x]!=color[adj[x][j]])
-                        z=1;
-                        else
-                        z=0;
-                        flag=flag&z;
-                    }
-                }
-                q.pop();
+                c=0;
+                goto end;
             }
-        }
+            if(!vis[y])
+            {
+                q.push(y);
+                vis[y]=1;
+                clr[y]=clr[z]^1;
+            }
+        }	
     }
-    if(flag==1)
-    cout<<"1"<<endl;
-    else
-    cout<<"0"<<endl;
-    return 0;
+	end:;
+	cout<<c;
+	return 0;
 }
