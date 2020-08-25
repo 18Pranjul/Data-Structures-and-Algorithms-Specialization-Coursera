254853683378eaf002ea5e815c3f654317169e68
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-26 00:01:04 +0530

    Update is_bst.cpp

--- Data Structures/week4_binary_search_trees/2_is_bst/is_bst.cpp
+++ Data Structures/week4_binary_search_trees/2_is_bst/is_bst.cpp
@@ -1,107 +1,52 @@
 #include <iostream>
-
 #include <algorithm>
-
 #include <vector>
-
 #include <math.h>
-
 #include <cstring>
-
 #include <string>
-
 #include <stack>
-
 #include <queue>
-
 #include <deque>
-
 #include <map>
-
 #include <set>
 #include <utility>
-
 #include <iomanip>
-
 #include <climits>
-
 using namespace std;
-
 #define ll long long
-
 #define MOD 1000000007
-
 #define MAX 1000000000000000000
-
 #define ln "\n"
-
 #define pb push_back
-
 #define pll pair<ll,ll>
-
+#define mp make_pair
 #define f first
 #define s second
-
-#define TEST ll t;cin>>t; while(t--)
-
+#define Test ll t;cin>>t; while(t--)
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
-
-struct roop
-
+struct node
 {
-
-    ll key;
-
-    ll left;
-
-    ll right;
-
+    ll key,left,right;
 };
-
-ll n;
-
-struct roop r[100001];
-
-ll flag=1;
-
-bool is_binary_search_tree(ll node,ll maxi,ll mini)
-
+ll c=1;
+void iscorrect(node a[],ll root,ll l,ll r)
 {
-
-    if((node<0) || (node>=n))
-
-    return true;
-
-    if((r[node].key<mini) || (r[node].key>maxi))
-
-    return false;
-
-    return is_binary_search_tree(r[node].left,r[node].key-1,mini)  && is_binary_search_tree(r[node].right,maxi,r[node].key+1);
-
+    if(!(a[root].key>l && a[root].key<r)) c=0;
+    if(a[root].left!=-1) iscorrect(a,a[root].left,l,a[root].key);
+    if(a[root].right!=-1) iscorrect(a,a[root].right,a[root].key,r);
 }
-
-int main()
-
+int main() 
 {
-
 	fast_io;
-
-	ll i;
-
+	ll n,i;
 	cin>>n;
-
-	for(i=0;i<n;i++)
-
-	cin>>r[i].key>>r[i].left>>r[i].right;
-
-    if(is_binary_search_tree(0,MAX,-MAX))
-
-    cout<<"CORRECT"<<endl;
-
+	node nd[n+5];
+	for(i=0;i<n;i++) cin>>nd[i].key>>nd[i].left>>nd[i].right;
+	if(n==0) cout<<"CORRECT";
 	else
-
-	cout<<"INCORRECT"<<endl;
-
-    return 0;
-
+	{
+	    iscorrect(nd,0,-MAX,MAX);
+	    cout<<(c?"CORRECT":"INCORRECT");
+	}
+	return 0;
 }
