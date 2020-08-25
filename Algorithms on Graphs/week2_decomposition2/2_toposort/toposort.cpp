95aaaf5920d41a33acb45fc194136859c81d6d05
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-26 00:04:11 +0530

    Update toposort.cpp

--- Algorithms on Graphs/week2_decomposition2/2_toposort/toposort.cpp
+++ Algorithms on Graphs/week2_decomposition2/2_toposort/toposort.cpp
@@ -21,52 +21,34 @@ using namespace std;
 #define pll pair<ll,ll>
 #define mp make_pair
 #define f first
-//#define s second
+#define s second
 #define Test ll t;cin>>t; while(t--)
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
-vector<ll> ans;
-vector<bool> visited;
-void dfs(vector<ll> adj[],ll s)
+stack <ll> st;
+void tsort(vector <ll> v[],ll vis[],ll k)
 {
-    visited[s]=true;
-    for (auto i:adj[s])
+    vis[k]=1;
+    for(ll i=0;i<v[k].size();i++)
     {
-        if(!visited[i])
-        dfs(adj,i);
+        ll y=v[k][i];
+        if(!vis[y]) tsort(v,vis,y);
     }
-    ans.pb(s);
-}
-void topological_sorting(vector<ll> adj[],ll n)
-{
-    visited.assign(n+1,false);
-    ans.clear();
-    for(ll i=1;i<=n;i++)
-    {
-        if(!visited[i])
-        {
-            dfs(adj,i);
-        }
-    }
-    reverse(ans.begin(),ans.end());
+    st.push(k);
 }
 int main() 
 {
 	fast_io;
-	ll n,m,i,s,a,b;
+	ll n,m,i;
 	cin>>n>>m;
-	vector<ll> adj[n+1];
-	visited.assign(n+1,false);
+	vector <ll> v[n+5];
 	for(i=0;i<m;i++)
 	{
+	    ll a,b;
 	    cin>>a>>b;
-	    adj[a].push_back(b);
-	    //adj[b].push_back(a);
+	    v[a].pb(b);
 	}
-	topological_sorting(adj,n);
-	for(auto i:ans)
-	{
-	    cout<<i<<" ";
-	}
-	cout<<endl;
+	ll vis[n+5]={0};
+	for(i=1;i<=n;i++) if(!vis[i]) tsort(v,vis,i);
+	while(st.size()) cout<<st.top()<<" ",st.pop();
 	return 0;
-} 
+}
