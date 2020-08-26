64b5f1d0a662b332b3a246aa973a5e2ffe3165c8
Pranjul Pal <46499642+18Pranjul@users.noreply.github.com>
2020-08-27 01:06:15 +0530

    Update bwt.cpp

--- Algorithm on Strings/Programming Assignment 2/bwt/bwt.cpp
+++ Algorithm on Strings/Programming Assignment 2/bwt/bwt.cpp
@@ -1,49 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
-typedef unsigned long long int ull;
-typedef long double ld;
-#define MOD 1000000007
-#define INF 1000000000000000000
-#define endll "\n"
+#define ll long long
+#define ln "\n"
 #define pb push_back
-#define forn(i,n) for(i=0;i<n;i++)
-#define forab(i,a,b) for(i=a;i<=b;i++)
-#define vpll vector<pair<ll,ll>>
 #define pll pair<ll,ll>
+#define ppll pair<ll,pll>
 #define vll vector<ll>
-#define ff first
-#define ss second
+#define vpll vector<pll>
+#define vvll vector<vector<ll>>
+#define sll stack<ll>
+#define qll queue<ll>
+#define mp make_pair
+#define f first
+#define s second
 #define bs binary_search
 #define lb lower_bound
 #define ub upper_bound
-#define test ll t;cin>>t; while(t--)
-#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-void consructBWT(string s)
+#define Test ll T;cin>>T; while(T--)
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define init(x,n,v) for(ll i=0;i<=n;i++) x[i]=v;
+#define all(x) x.begin(),x.end()
+#define pi 3.14159265358979323846
+ll MOD = 1e9+7 , MAX = 1e18;
+int main() 
 {
-  vector<string> v;
-  ll l=s.length();
-  s+=s;
-  ll i;
-  for(i=0;i<l;i++)
-  {
-      string s1=s.substr(i,l);
-      v.pb(s1);
-  }
-  sort(v.begin(),v.end());
-  for(i=0;i<v.size();i++)
-  {
-    cout<<v[i][l-1];
-  }
+	fast_io;
+	string s;
+	cin>>s;
+	set <string> ss;
+	ll n = s.size() , i=0;
+	while(i<n)
+	{
+	    s.insert(s.begin(),s[n-1]);
+	    s.erase(s.begin()+n);
+	    ss.insert(s);
+	    i++;
+	}
+	while(ss.size())
+	{
+	    string k = *ss.begin();
+	    ss.erase(ss.begin());
+	    cout<<k[n-1];
+	}
+	return 0;
 }
-
-int main()
-{
-  fast_io;
-  string s;
-  cin>>s;
-  consructBWT(s);
-  return 0;
-}
-
